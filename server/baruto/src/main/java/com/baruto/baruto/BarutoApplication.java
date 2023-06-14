d6c5a203a3d5f214b609dea8a0d036a427ce91c9
Seohabin6078 <habin6078@gmail.com>
2023-06-14 17:08:54 +0900

    server initial commit

---
+++ server/baruto/src/main/java/com/baruto/baruto/BarutoApplication.java
@@ -0,0 +1,13 @@
+package com.baruto.baruto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarutoApplication.class, args);
+	}
+
+}
